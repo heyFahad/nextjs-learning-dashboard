187233193896aeacc9f4b7911b5cc639b4656b94
Fahad Javed <fahad.javed1@arbisoft.com>
2024-07-02 18:41:03 +0500

    refactor: Add useDebouncedCallback for search functionality in Search component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,14 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
 
     // update the query params with the new search term
@@ -20,7 +21,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
     // update route with the newer query params
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
